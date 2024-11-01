3c26a48b42749d5aba4799168bffa15bcbb9e4e1
EdgarFx <120090414@link.cuhk.edu.cn>
2024-11-01 00:24:29 -0400

    fx

--- QuickTable/quicktable.py
+++ QuickTable/quicktable.py
@@ -44,12 +44,12 @@ gci(file)
 # 接下来就是对images里生成的图片先进行表格检测，这一步不需要区分中英文（用的都可以的模型）
 image_files = os.listdir('images')
 for fi in image_files:
-    os.system('python Detection/deploy/infer.py --model_dir=Detection/output_inference/picodet_lcnet_x1_0_fgd_layout_table_infer/ \
-    --image_file=images/%s --device=CPU --output_dir=Detection/det_output/ --save_results'%fi)
+    os.system('python detection/deploy/infer.py --model_dir=detection/output_inference/picodet_lcnet_x1_0_fgd_layout_table_infer/ \
+    --image_file=images/%s --device=CPU --output_dir=detection/det_output/ --save_results'%fi)
 
 
 # 然后，切割得到表格图像
-os.system('python Detection/deploy/cut.py')
+os.system('python detection/deploy/cut.py')
 
 
 # 接下来，即为表格结构识别和文本识别，并将得到excel文件，此时要判断中英文，由用户指定
@@ -60,20 +60,20 @@ tables = os.listdir("det_tables")
 
 for table in tables:
     if language == "Chinese":
-        os.system('python TableStructure/ppstructure/table/predict_table.py \
-            --det_model_dir=TableStructure/ppstructure/inference/ch_PP-OCRv3_det_infer \
-            --rec_model_dir=TableStructure/ppstructure/inference/ch_PP-OCRv3_rec_infer \
-            --table_model_dir=TableStructure/ppstructure/inference/ch_ppstructure_mobile_v2.0_SLANet_infer \
-            --rec_char_dict_path=TableStructure/ppocr/utils/ppocr_keys_v1.txt \
-            --table_char_dict_path=TableStructure/ppocr/utils/dict/table_structure_dict_ch.txt \
+        os.system('python table_structure/ppstructure/table/predict_table.py \
+            --det_model_dir=table_structure/ppstructure/inference/ch_PP-OCRv3_det_infer \
+            --rec_model_dir=table_structure/ppstructure/inference/ch_PP-OCRv3_rec_infer \
+            --table_model_dir=table_structure/ppstructure/inference/ch_ppstructure_mobile_v2.0_SLANet_infer \
+            --rec_char_dict_path=table_structure/ppocr/utils/ppocr_keys_v1.txt \
+            --table_char_dict_path=table_structure/ppocr/utils/dict/table_structure_dict_ch.txt \
             --image_dir=det_tables/%s --output=output/%s'%(table,table[:-4]))
     elif language == "English":
-        os.system('python TableStructure/ppstructure/table/predict_table.py \
-            --det_model_dir=TableStructure/ppstructure/inference/en_PP-OCRv3_det_infer \
-            --rec_model_dir=TableStructure/ppstructure/inference/en_PP-OCRv3_rec_infer \
-            --table_model_dir=TableStructure/ppstructure/inference/en_ppstructure_mobile_v2.0_SLANet_infer \
-            --rec_char_dict_path=TableStructure/ppocr/utils/ppocr_keys_v1.txt \
-            --table_char_dict_path=TableStructure/ppocr/utils/dict/table_structure_dict_ch.txt \
+        os.system('python table_structure/ppstructure/table/predict_table.py \
+            --det_model_dir=table_structure/ppstructure/inference/en_PP-OCRv3_det_infer \
+            --rec_model_dir=table_structure/ppstructure/inference/en_PP-OCRv3_rec_infer \
+            --table_model_dir=table_structure/ppstructure/inference/en_ppstructure_mobile_v2.0_SLANet_infer \
+            --rec_char_dict_path=table_structure/ppocr/utils/ppocr_keys_v1.txt \
+            --table_char_dict_path=table_structure/ppocr/utils/dict/table_structure_dict_ch.txt \
             --image_dir=det_tables/%s --output=output/%s'%(table,table[:-4]))
 
 
